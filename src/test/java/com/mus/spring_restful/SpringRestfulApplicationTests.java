033066a88ea83e30334738d47f40e1d99ca8a2fc
Umasankar Mariyappan <mustechbox@gmail.com>
2021-08-28 22:05:26 +0530

    Initial commit

---
+++ src/test/java/com/mus/spring_restful/SpringRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mus.spring_restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
